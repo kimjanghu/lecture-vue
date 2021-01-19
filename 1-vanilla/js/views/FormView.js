97c15ebbc8682088ce2de1198200df241fec252c
kimjanghu <kjh921030@gmail.com>
2021-01-19 15:22:45 +0900

    Add reset btn

--- 1-vanilla/js/views/FormView.js
+++ 1-vanilla/js/views/FormView.js
@@ -7,13 +7,23 @@ const FormView = Object.create(View)
 FormView.setup = function (el) {
   this.init(el)
   this.inputEl = el.querySelector('[type=text]')
-  this.resetEl = el.querySelector('[type=reset')
+  this.resetEl = el.querySelector('[type=reset]')
   this.showResetBtn(false)
+  this.bindEvents()
   return this
 }
 
 FormView.showResetBtn = function (show = true) {
   this.resetEl.style.display = show ? 'block' : 'none'
 }
+
+FormView.bindEvents = function () {
+  this.inputEl.addEventListener('keyup', e => this.onKeyup())
+}
+
+FormView.onKeyup = function () {
+  this.showResetBtn(this.inputEl.value.length)
+}
+
 
 export default FormView
