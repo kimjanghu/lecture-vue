1e1293c986538431d3ac51f9f10d87541bbc4f81
kimjanghu <kjh921030@gmail.com>
2021-01-19 16:30:31 +0900

    Practice form view

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -6,9 +6,14 @@ export default {
   init() {
     FormView.setup(document.querySelector('form'))
       .on('@submit', e => this.onSubmit(e.detail.input))
+      .on('@reset', () => this.onClickReset())
   },
   
   onSubmit(input) {
     console.log(tag, 'onSubmit()', input)
   },
+
+  onClickReset() {
+    console.log(tag, 'onClickReset()')
+  }
 }
