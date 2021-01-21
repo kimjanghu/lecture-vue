88a44c86aa39d1a60b6743ec572ceee5267d3831
kimjanghu <kjh921030@gmail.com>
2021-01-21 22:32:20 +0900

    Add tab view

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -1,8 +1,10 @@
 import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
 import TabView from '../views/TabView.js'
+import KeywordView from '../views/KeywordView.js'
 
 import SearchModel from '../models/SearchModel.js'
+import KeywordModel from '../models/KeywordModel.js'
 
 const tag = '[MainController]'
 
@@ -17,6 +19,8 @@ export default {
 
     ResultView.setup(document.querySelector('#search-result'))
 
+    KeywordView.setup(document.querySelector('#search-keywords'))
+
     this.selectedTab = '추천 검색어'
     this.renderView()
   },
@@ -25,7 +29,19 @@ export default {
     console.log(tag, 'rednerView()')
     TabView.setActiveTab(this.selectedTab)
 
+    if (this.selectedTab === '추천 검색어') {
+      this.fetchSearchKeyword()
+    } else {
+
+    }
+
     ResultView.hide()
+  },
+
+  fetchSearchKeyword() {
+    KeywordModel.list().then(data => {
+      KeywordView.render(data)
+    })
   },
 
   search(query) {
