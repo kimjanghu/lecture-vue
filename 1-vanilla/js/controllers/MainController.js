cbef7bc05d1616fce55de02a6b5717aa02f804cf
kimjanghu <kjh921030@gmail.com>
2021-01-23 00:07:55 +0900

    Add History view

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -28,7 +28,7 @@ export default {
 
     ResultView.setup(document.querySelector('#search-result'))
 
-    this.selectedTab = '최근 검색어'
+    this.selectedTab = '추천 검색어'
     this.renderView()
   },
 
@@ -84,7 +84,8 @@ export default {
   },
 
   onChangeTab(tabName) {
-    debugger
+    this.selectedTab = tabName
+    this.renderView()
   },
 
   onClickKeyword(keyword) {
