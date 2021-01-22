deb4d5e42f1d43a6146348ee79676e3978c0e04e
kimjanghu <kjh921030@gmail.com>
2021-01-22 23:47:19 +0900

    Add keyword view

--- 1-vanilla/js/controllers/MainController.js
+++ 1-vanilla/js/controllers/MainController.js
@@ -2,9 +2,11 @@ import FormView from '../views/FormView.js'
 import ResultView from '../views/ResultView.js'
 import TabView from '../views/TabView.js'
 import KeywordView from '../views/KeywordView.js'
+import HistoryView from '../views/HistoryView.js'
 
 import SearchModel from '../models/SearchModel.js'
 import KeywordModel from '../models/KeywordModel.js'
+import HistoryModel from '../models/HistoryModel.js'
 
 const tag = '[MainController]'
 
@@ -19,10 +21,13 @@ export default {
 
     KeywordView.setup(document.querySelector('#search-keyword'))
       .on('@click', e => this.onClickKeyword(e.detail.keyword))
+    
+    HistoryView.setup(document.querySelector('#search-history'))
+      .on('@click', e => this.onClickHistory(e.detail.keyword))
 
     ResultView.setup(document.querySelector('#search-result'))
 
-    this.selectedTab = '추천 검색어'
+    this.selectedTab = '최근 검색어'
     this.renderView()
   },
 
@@ -33,7 +38,7 @@ export default {
     if (this.selectedTab === '추천 검색어') {
       this.fetchSearchKeyword()
     } else {
-      debugger
+      this.fetchSearchHistory()
     }
 
     ResultView.hide()
@@ -42,6 +47,12 @@ export default {
   fetchSearchKeyword() {
     KeywordModel.list().then(data => {
       KeywordView.render(data)
+    })
+  },
+
+  fetchSearchHistory() {
+    HistoryModel.list().then(data => {
+      HistoryView.render(data)
     })
   },
 
@@ -73,6 +84,10 @@ export default {
   },
 
   onClickKeyword(keyword) {
+    this.search(keyword)
+  },
+
+  onClickHistory(keyword) {
     this.search(keyword)
   }
 }
